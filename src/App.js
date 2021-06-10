6ef6dbe6468fc3fdc88249fe69292170eaacf43c
Eryk Słociński <erq.programmer@gmail.com>
2021-06-10 17:26:03 +0200

    Added material-ui, react-query for fetch data and created simple ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from 'react-query';
 
-function App() {
+import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+import { Wrapper, StyledCircularProgress, StyledButton } from './App.styles';
+
+const getProducts = async () =>
+  await (
+    await fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+  ).json();
+
+const App = () => {
+  const { data, error, isLoading, isError } = useQuery('products', getProducts);
+  console.log(data);
+
+  const handleAddButton = (product) => console.log(product);
+
+  if (isLoading) {
+    return <StyledCircularProgress />;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <h1>Oferta cateringowa</h1>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          {data?.map((item) => (
+            <div key={item.category.name}>
+              <h2>{item.category.name}</h2>
+              <p>{item.category.description}</p>
+              <List>
+                {item.content.map((product) => (
+                  <ListItem key={product.id}>
+                    <ListItemText>
+                      {product.name} <strong>{product.price} zł</strong>
+                    </ListItemText>
+                    <Button
+                      onClick={() =>
+                        handleAddButton({
+                          ...product,
+                          category: item.category.name,
+                        })
+                      }
+                    >
+                      Add
+                    </Button>
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          ))}
+        </Grid>
+      </Grid>
+    </Wrapper>
   );
-}
+};
 
 export default App;
---
+++ src/App.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import IconButton from '@material-ui/core/IconButton';
+
+export const StyledCircularProgress = styled(CircularProgress)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Wrapper = styled.div`
+  margin: 80px 40px;
+`;
+
+export const StyledButton = styled(IconButton)`
+  position: fixed;
+  z-index: 100;
+  right: 20px;
+  top: 20px;
+`;
+
+export const ListColor = styled.div`
+  background-color: red;
+`;
