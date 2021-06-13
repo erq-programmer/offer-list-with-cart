828799489ad3c937803e18733dcdf6168521334c
Eryk Słociński <erq.programmer@gmail.com>
2021-06-13 19:15:20 +0200

    Added total products price for Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,19 +6,28 @@ import { groupBy } from '../../helpers';
 const Cart = ({ cartItems, addToCart, removeFromCart }) => {
   const productGroupedByCategory = groupBy(cartItems, 'category');
 
+  const calculateTotalPrice = (product) =>
+    product.reduce((acc, product) => acc + product.amount * product.price, 0);
+
   return (
     <Wrapper>
       <div>
         <h2>Zamówienie cateringowe</h2>
         {cartItems.length === 0 ? <p>Brak produktów w zamówieniu</p> : null}
-        {Object.entries(productGroupedByCategory)?.map(([key, value]) => (
-          <CartCategory
-            category={key}
-            products={value}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-          />
-        ))}
+        {Object.entries(productGroupedByCategory)?.map(
+          ([category, categoryData]) => (
+            <CartCategory
+              key={category}
+              category={category}
+              products={categoryData}
+              addToCart={addToCart}
+              removeFromCart={removeFromCart}
+            />
+          )
+        )}
+        <p>
+          <strong>Cena za catering: {calculateTotalPrice(cartItems)} zł</strong>
+        </p>
       </div>
     </Wrapper>
   );
